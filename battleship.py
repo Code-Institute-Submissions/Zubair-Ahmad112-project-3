ff353ae0f4c67a7daf1cd05fe4dc4101212ba562
Zubair Ahmad <130005005+Zubair-Ahmad112@users.noreply.github.com>
2023-04-29 17:23:43 +0500

    Placement of ships

--- battleship.py
+++ battleship.py
@@ -82,3 +82,61 @@ def print_pc_battlefield():
 
 print_pc_battlefield()
 print_battlefield()
+
+# Place ships
+for ship in ships:
+    print(f"Placing {ship['name']}...")
+    while True:
+        direction = input("Enter the direction (H/V): ")
+        if direction.upper() != "H" and direction.upper() != "V":
+            print("Please enter H or V.")
+            continue
+   
+        x = input("Enter the x coordinate: ")
+
+        x = x.upper()
+        x = ord(x)
+        x = x - 65
+        if x < 0 or x > size - 1:
+            print(f"Please enter a letter between A and {chr(size + 64)}.")
+            continue
+   
+        y = input("Enter the y coordinate: ")
+
+        y = y.upper()
+        y = ord(y)
+        y = y - 65
+        if y < 0 or y > size - 1:
+            print(f"Please enter a letter between A and {chr(size + 64)}.")
+            continue
+   
+        if direction.upper() == "H":
+            if x + ship["size"] > size:
+                print(f"Please enter a letter between A and {chr(size + 65 - ship['size'])}.")
+                continue
+            retry = False
+            for i in range(ship["size"]):
+                if battlefield[y][x + i] != "O":
+                    print("There is already a ship there.")
+                    retry = True
+                    break
+            if retry:
+                continue
+            for i in range(ship["size"]):
+                battlefield[y][x + i] = ship["symbol"]
+        else:
+            if y + ship["size"] > size:
+                print(f"Please enter a letter between A and {chr(size + 65 - ship['size'])}.")
+                continue
+            retry = False
+            for i in range(ship["size"]):
+                if battlefield[y + i][x] != "O":
+                    print("There is already a ship there.")
+                    retry = True
+                    break
+            if retry:
+                continue
+            for i in range(ship["size"]):
+                battlefield[y + i][x] = ship["symbol"]
+        print_battlefield()
+        break
